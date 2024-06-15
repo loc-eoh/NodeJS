d95e1d32c01c81b2cafbda9cb6dc2f6da89bf968
loc-eoh <root@PhuocLoc>
2024-06-15 12:03:31 +0700

    first

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    var a = 1
-    var b = 2
-    var c = a + b
-    return res.send('Xin chao')
-})//route
+app.get("/", (req, res) => res.send("chao ban")); //route
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
